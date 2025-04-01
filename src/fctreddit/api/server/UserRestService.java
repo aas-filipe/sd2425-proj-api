5f60b7cdddfaf9e5ac8f448d2672481f8ac7c37b
Carpetess <andre.filipe19062003@gmail.com>
2025-04-01 12:01:04 +0100

    User enpoint started

--- src/fctreddit/api/server/UserRestService.java
+++ src/fctreddit/api/server/UserRestService.java
@@ -3,6 +3,8 @@ import fctreddit.api.User;
 import fctreddit.api.java.Result;
 import fctreddit.api.rest.RestUsers;
 import fctreddit.api.server.persistence.Hibernate;
+import jakarta.ws.rs.WebApplicationException;
+import jakarta.ws.rs.core.Response;
 
 import java.util.List;
 import java.util.logging.Logger;
@@ -16,33 +18,71 @@ public class UserRestService implements RestUsers {
     public UserRestService() { this.hibernate = Hibernate.getInstance(); }
 
     @Override
-    public String createUser(User user) {
+    public String createUser(User user) throws WebApplicationException {
         User u = hibernate.get(User.class, user.getUserId());
         if (u != null) {
-            return Result;
+            throw new WebApplicationException(Response.Status.CONFLICT);
+        }
+        //TODO: BAD REQUEST RESPONSE
+        try {
+            hibernate.persist(user);
+        } catch (Exception e) {
+            throw new WebApplicationException(Response.Status.INTERNAL_SERVER_ERROR);
         }
 
-
-        return "";
+        return user.getUserId();
     }
 
     @Override
     public User getUser(String userId, String password) {
-        return null;
+        User u = authenticateUser(userId, password);
+
+        logger.fine("User with id " + userId + " found");
+        return u;
     }
 
     @Override
     public User updateUser(String userId, String password, User user) {
-        return null;
+        authenticateUser(userId, password);
+
+        try {
+            hibernate.persist(user);
+        } catch (Exception e) {
+            throw new WebApplicationException(Response.Status.INTERNAL_SERVER_ERROR);
+        }
+
+        return user;
+
     }
 
     @Override
     public User deleteUser(String userId, String password) {
-        return null;
+        User u = authenticateUser(userId, password);
+
+        try {
+            hibernate.delete(u);
+        } catch (Exception e) {
+            throw new WebApplicationException(Response.Status.INTERNAL_SERVER_ERROR);
+        }
+        return u;
+    }
+
+    private User authenticateUser(String userId, String password) {
+        User u = hibernate.get(User.class, userId);
+        if (u == null) {
+            logger.warning("User with id " + userId + " not found");
+            throw new WebApplicationException(Response.Status.NOT_FOUND);
+        }
+        if (!u.getPassword().equals(password)) {
+            logger.warning("Password does not match");
+            throw new WebApplicationException(Response.Status.FORBIDDEN);
+        }
+        return u;
     }
 
     @Override
     public List<User> searchUsers(String pattern) {
+        // TODO : JPQL QUERY
         return List.of();
     }
 }
