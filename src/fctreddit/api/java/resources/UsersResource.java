2b634b3e814cd6660b3fa9d9323a59d8597815c8
Carpetess <andre.filipe19062003@gmail.com>
2025-04-08 12:51:23 +0100

    Finished UserResource

--- src/fctreddit/api/java/resources/UsersResource.java
+++ src/fctreddit/api/java/resources/UsersResource.java
@@ -18,40 +18,80 @@ public class UsersResource implements Users {
     private static final String SERVICE_PROTOCOL = "/rest";
 
     private static Logger Log = Logger.getLogger(UsersResource.class.getName());
-    private ImageClientClass imagesClient;
-    private ContentClientClass contentClient;
     private Hibernate hibernate;
 
     public UsersResource() throws IOException {
         Hibernate hibernate = Hibernate.getInstance();
         Discovery discovery = new Discovery(Discovery.DISCOVERY_ADDR, SERVICE_NAME, InetAddress.getLocalHost().getHostAddress() + SERVICE_PROTOCOL);
         discovery.start();
-        this.imagesClient = new ImageClientClass(discovery);
-        this.contentClient = new ContentClientClass(discovery);
     }
 
     @Override
     public Result<String> createUser(User user) {
-        return null;
+        if(user == null) {
+            return Result.error(Result.ErrorCode.BAD_REQUEST);
+        }
+        if(hibernate.get(User.class, user.getUserId()) != null) {
+            return Result.error(Result.ErrorCode.CONFLICT);
+        }
+        try {
+            hibernate.persist(user);
+        } catch (Exception e) {
+            return Result.error(Result.ErrorCode.INTERNAL_ERROR);
+        }
+        return Result.ok(user.getUserId());
     }
 
     @Override
     public Result<User> getUser(String userId, String password) {
-        return null;
+        if(userId == null || password == null) {
+            return Result.error(Result.ErrorCode.BAD_REQUEST);
+        }
+        User user = hibernate.get(User.class, userId);
+        if(user == null) {
+            return Result.error(Result.ErrorCode.NOT_FOUND);
+        }
+        if(!password.equals(user.getPassword())) {
+            return Result.error(Result.ErrorCode.FORBIDDEN);
+        }
+        return Result.ok(user);
     }
 
     @Override
     public Result<User> updateUser(String userId, String password, User user) {
-        return null;
+        if(userId == null || password == null) {
+            return Result.error(Result.ErrorCode.BAD_REQUEST);
+        }
+        User oldUser = hibernate.get(User.class, userId);
+        if(oldUser == null) {
+            return Result.error(Result.ErrorCode.NOT_FOUND);
+        }
+        if(!password.equals(oldUser.getPassword())) {
+            return Result.error(Result.ErrorCode.FORBIDDEN);
+        }
+        hibernate.update(user);
+        return Result.ok(user);
     }
 
     @Override
     public Result<User> deleteUser(String userId, String password) {
-        return null;
+        if(userId == null || password == null) {
+            return Result.error(Result.ErrorCode.BAD_REQUEST);
+        }
+        User user = hibernate.get(User.class, userId);
+        if(user == null) {
+            return Result.error(Result.ErrorCode.NOT_FOUND);
+        }
+        if(!password.equals(user.getPassword())) {
+            return Result.error(Result.ErrorCode.FORBIDDEN);
+        }
+        hibernate.delete(user);
+        return Result.ok(user);
     }
 
     @Override
     public Result<List<User>> searchUsers(String pattern) {
+        //TODO : JPQL querry, provavelmente usar chatgpt seria a way, ou ver os ppts
         return null;
     }
 }
